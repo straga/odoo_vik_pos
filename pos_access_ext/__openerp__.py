9a00568722acae779021dd961d5b06d4d680f66f
straga <vostraga@gmail.com>
2017-02-15 11:20:40 +0200

    DOC: update

--- pos_access_ext/__openerp__.py
+++ pos_access_ext/__openerp__.py
@@ -1,34 +1,50 @@
 # -*- coding: utf-8 -*-
-
 {
-    'name': 'POS access extended',
-    'version': '1.0',
-    'category': 'Point of Sale',
-    'sequence': 6,
-    'summary': 'Access extended Discount and Price for the Point of Sale ',
-    'description': """
+    "name": """POS Access Code""",
+    "summary": """Access extended - Discount and Price for the Point of Sale""",
+    "category": "Point of Sale",
+    "images": ['static/description/icon.png'],
+    "version": "1.0.0",
 
-=======================
+    "description": """
 
-This module adds constol access to Discount and Price features to the Point of Sale:
+    =======================
 
+    This module adds constol access to Discount and Price features to the Point of Sale:
 
-""",
-    'author': 'Viktor Vorobjov',
-    'depends': ['point_of_sale'],
+    """,
 
-    'license': 'LGPL-3',
-    'website': 'https://straga.github.io',
-    'support': 'vostraga@gmail.com',
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
 
-    'data': [
+    "depends": [
+        "point_of_sale",
+        "pos_adv_discount"
+
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
         'views/templates.xml',
         'views/views.xml',
     ],
-    'qweb':[
+    "qweb": [
 
         'static/src/xml/access_ext.xml',
     ],
-    'installable': True,
-    'auto_install': False,
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
+
+
+
+
+
+
