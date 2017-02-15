ddf5cd39e14c42bc8d54400e0f5e3779f36ba48b
straga <vostraga@gmail.com>
2017-02-15 11:02:56 +0200

    DOC: update

--- pos_adv_discount/__openerp__.py
+++ pos_adv_discount/__openerp__.py
@@ -1,38 +1,48 @@
 # -*- coding: utf-8 -*-
-
-
 {
-    'name': 'POS Advance Discount',
-    'version': '1.0',
-    'category': 'Point of Sale',
-    'sequence': 6,
-    'summary': 'POS Advance Discount for the Point of Sale ',
-    'description': """
+    "name": """POS Advance Discount""",
+    "summary": """POS Advance Discount for the Point of Sale""",
+    "category": "Point of Sale",
+    "images": ['static/description/icon.png'],
+    "version": "1.0.0",
 
-=======================
+    "description": """
 
-This module adds POS Advance Discount features to the Point of Sale:
+    =======================
 
+    This module adds POS Advance Discount features to the Point of Sale:
 
-""",
-    'author': 'Viktor Vorobjov',
-    'depends': ['point_of_sale'],
+    """,
 
-    'license': 'LGPL-3',
-    'website': 'https://straga.github.io',
-    'support': 'vostraga@gmail.com',
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
 
-    'data': [
+    "depends": [
+        "point_of_sale",
+
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
         'views/templates.xml',
         'pos_adv_discount_views.xml',
         'pos_order_in_views.xml',
         'security/ir.model.access.csv',
     ],
-    'qweb':[
+    "qweb": [
 
         'static/src/xml/adv_discount.xml',
     ],
-    'installable': True,
-    'auto_install': False,
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
+
+
 
