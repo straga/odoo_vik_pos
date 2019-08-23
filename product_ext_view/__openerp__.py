9851adcf1821ee33489c4af4d1c8aeb1f4c0b199
straga <vostraga@gmail.com>
2019-08-23 13:53:07 +0300

    deacriotuins update

--- product_ext_view/__openerp__.py
+++ product_ext_view/__openerp__.py
@@ -1,35 +1,42 @@
+
 # -*- coding: utf-8 -*-
 {
-    'name': "product_ext_view",
+    "name": "product_ext_view",
 
-    'summary': """
+    "summary": """
         Extended Product Tree View.
         Add:  "POS category" and "Price".
         Remove:  "status" and "product type".
         """,
 
-    'description': """
+    "description": """
          Extended Product Tree View
     """,
 
-    'author': "Viktor Vorobjov",
-    'license': 'LGPL-3',s
-    'website': 'https://straga.github.io',
-    'support': 'vostraga@gmail.com',
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
 
-    'category': 'Uncategorized',
-    'version': '0.1',
+    "depends": [
+        'base', 'product', 'point_of_sale'
 
-    # any module necessary for this one to work correctly
-    'depends': ['base','product', 'point_of_sale'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "templates.xml",
     ],
+    "qweb": [
+
+    ],
+    "demo": [
+        "demo.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
